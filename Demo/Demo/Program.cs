1ead8d4cd18de4d8c015d9d7d140a42fb512598e
banan-alwahidi <97317208+banan-alwahidi@users.noreply.github.com>
2023-12-18 10:31:52 +0300

    Add files via upload

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class program
+{
+    public static void Main()
+    {
+        // value type : built-in
+        int number;
+        Console.WriteLine("Enter your number");
+        number=int.Parse(Console.ReadLine());
+        Console.WriteLine(number);
+        // Refernce type : built-in
+        int[] nums = new int[number];
+        for(int i = 0; i < nums.Length; i++)
+        {
+            nums[i] =int.Parse(Console.ReadLine());
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.WriteLine(nums[i]);
+        }
+    }
+}
